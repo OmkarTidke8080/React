e0032f31cd2c49ba74b02b00f59f05cdc9062619
Omkar Ashok Tidke <132737733+OmkarTidke8080@users.noreply.github.com>
2024-03-06 07:42:53 +0530

    Add files via upload

---
+++ project_1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <>
+   
+<Navbar title="TextUtils" aboutText="About TextUtils"/>  (// this is prop)
+{/* <Navbar />   */}\
+<Navbar title="TextUtils" />
+    </>
+  );
+}
+
+export default App;
